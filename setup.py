c6e6b885ee1c59ef7ca1580e518683874f8d387d
vishwas <150580398+2bit007@users.noreply.github.com>
2024-02-01 12:05:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     version=__import__('xnLinkFinder').__version__,
     description="A python script to find endpoints from a URL, a file of URLs, a directory of files, a Burp XML file or a ZAP ASCII message file. It also gets potential parameters and a target specific wordlist.",
     long_description=open("README.md").read(),
-    author="@xnl-h4ck3r",
-    url="https://github.com/xnl-h4ck3r/xnlLinkFinder",
+    author="2bit007",
+    url="https://github.com/2bit007/xnlLinkFinder",
     py_modules=["xnLinkFinder"],
     install_requires=["argparse","requests","psutil","pyyaml","termcolor","urlparse3","beautifulsoup4","lxml","html5lib","urllib3"],
     entry_points={
